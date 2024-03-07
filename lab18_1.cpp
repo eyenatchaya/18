9bb13f71ccabad7d022cf3b80efc35377b9ef5da
eye <eyenathaya5@gmail.com>
2024-03-07 17:17:56 +0700

    complete

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -6,9 +6,40 @@ struct Rect{
 };
 
 double overlap(Rect R1, Rect R2){
-    double x_dist = min(R1.x + R1.w, R2.x + R2.w) - max(R1.x, R2.x);
-    double y_dist = min(R1.y,R2.y) - max(R1.y-R1.h,R2.y-R2.h);
-    if(x_dist <= 0)
-	return 0;
-    return x_dist * y_dist;
+    double xLeft, xRight, yTop, yBottom, xLength, yLength, Area;
+
+    if(R1.x > R2.x) xLeft = R1.x;
+    else xLeft = R2.x;
+
+    if(R1.x + R1.w < R2.x + R2.w) xRight = R1.x + R1.w;
+    else xRight = R2.x + R2.w;
+
+    if(R1.y < R2.y) yTop = R1.y;
+    else yTop = R2.y;
+
+    if(R1.y - R1.h > R2.y - R2.h) yBottom = R1.y - R1.h;
+    else yBottom = R2.y - R2.h;
+
+    xLength = xRight - xLeft;
+    yLength = yTop - yBottom;
+
+    if((xLeft > xRight) || (yBottom > yTop)) return 0;
+    else{
+        Area = xLength * yLength;
+        return Area;
+    }
 }
+    
+
+int main(){
+    Rect R1 = {-1,2,6.9,9.6};
+    Rect R2 = {0,0,1.2,2.5};	
+    cout << overlap(R1,R2);	
+}
+// double overlap(Rect R1, Rect R2){
+//     double x_dist = min(R1.x + R1.w, R2.x + R2.w) - max(R1.x, R2.x);
+//     double y_dist = min(R1.y,R2.y) - max(R1.y-R1.h,R2.y-R2.h);
+//     if(x_dist <= 0)
+// 	return 0;
+//     return x_dist * y_dist;
+// }
